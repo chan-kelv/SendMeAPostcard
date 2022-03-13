d280f668bcef658a498bb860091c00aae4fe8676
chan-kelv <chan.kelv@gmail.com>
2022-03-13 00:57:30 -0800

    splash remember login flow

--- app/src/main/java/com/kelvin/postcardz/ui/auth/splash/SplashFragment.kt
+++ app/src/main/java/com/kelvin/postcardz/ui/auth/splash/SplashFragment.kt
@@ -1,14 +1,24 @@
 package com.kelvin.postcardz.ui.auth.splash
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewModelScope
 import com.kelvin.postcardz.databinding.FragmentSplashLoadingBinding
+import com.kelvin.postcardz.ui.HomeActivity
+import com.kelvin.postcardz.ui.auth.AuthLandingFragment
 import com.kelvin.postcardz.ui.auth.AuthLandingFragment.Companion.navigateToAuthLandingFragment
+import com.kelvin.postcardz.ui.auth.setupViewBindings
 import com.kelvin.postcardz.ui.base.PostcardBaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashFragment : PostcardBaseFragment() {
@@ -23,13 +33,29 @@ class SplashFragment : PostcardBaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         _splashViewBinding = FragmentSplashLoadingBinding.inflate(inflater, container, false)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    splashViewModel.foundAuthUser.collect { userFound ->
+                        if (userFound) {
+                            val intent = Intent(requireActivity(), HomeActivity::class.java)
+                            startActivity(intent)
+                            requireActivity().finish()
+                        } else {
+                            navigateToAuthLandingFragment()
+                        }
+                    }
+                }
+            }
+        }
         return splashViewBinding.root
     }
 
     override fun onResume() {
         super.onResume()
-        if (splashViewModel.isUserSetup()) {
-            navigateToAuthLandingFragment()
+        splashViewModel.viewModelScope.launch {
+            splashViewModel.attemptRetrieveUser()
         }
     }
 
--- app/src/main/java/com/kelvin/postcardz/ui/auth/splash/SplashViewModel.kt
+++ app/src/main/java/com/kelvin/postcardz/ui/auth/splash/SplashViewModel.kt
@@ -1,16 +1,48 @@
 package com.kelvin.postcardz.ui.auth.splash
 
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.gson.Gson
+import com.kelvin.postcardz.data.UserManager
+import com.kelvin.postcardz.data.model.UserProfile
 import com.kelvin.postcardz.data.preference.UserSettingsSharedPrefManager
 import com.kelvin.postcardz.ui.base.PostcardBaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val splashRepo: SplashRepository
-): PostcardBaseViewModel(){
-    fun isUserSetup(): Boolean {
-        return splashRepo.isUserSignedIn()
+    private val userManager: UserManager
+) : PostcardBaseViewModel() {
+    private val _foundAuthUser = MutableSharedFlow<Boolean>()
+    val foundAuthUser = _foundAuthUser.asSharedFlow()
+
+    suspend fun attemptRetrieveUser() {
+        val fbUser = FirebaseAuth.getInstance().currentUser
+        if (fbUser != null) {
+            val fbDatabase =
+                FirebaseDatabase.getInstance().getReference("ID_USERPROFILE").child(fbUser.uid)
+                    .get().addOnSuccessListener {
+                        val userProfile = Gson().fromJson(it.value.toString(), UserProfile::class.java)
+                        if (userProfile != null) {
+                            userManager.currentUser = userProfile
+                        }
+
+                        viewModelScope.launch {
+                            _foundAuthUser.emit(userProfile != null)
+                        }
+                    }.addOnFailureListener {
+                        viewModelScope.launch {
+                            _foundAuthUser.emit(false)
+                        }
+                    }
+        } else {
+            _foundAuthUser.emit(false)
+        }
     }
 
     // TODO simple repo not worth making its own class for now?
